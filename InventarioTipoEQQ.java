235e3c5349a434ebb5d831f88418b6a6ad1ade1d
JOSUE-JIMENEZ-73 <jimenezlaptop08@gmail.com>
2025-11-05 19:28:44 -0600

    Agregaron comentarios descriptivos de cada variable y variables iniciales en la clase

--- InventarioTipoEQQ.java
+++ InventarioTipoEQQ.java
@@ -1,6 +1,27 @@
+
+/*
+ * d (Demanda)
+ * k (costoPedido)
+ * h (Costo de almaenamiento)
+ * y (Cantidad optima de pedido)
+ * L (Tiempo de entrega)
+ * to (tiempo entre pedido)
+ * n (numero de ciclos de pedido)
+ * le (Punto de reorden)
+ * TCU (Costo total de inventario)
+ */
+import java.util.Scanner;
+
 public class InventarioTipoEQQ {
 
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int d, k,y,L,to,n,le,TCU;
+        float h;
         
+
+
+
+
     }
 }
